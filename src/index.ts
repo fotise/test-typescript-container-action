72a37083a1e7eeb3992b8b559b47910531ceed7b
fotise <58773095+fotise@users.noreply.github.com>
2020-02-17 15:27:07 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,8 @@ import { getInput } from "@actions/core";
 const name = getInput("name", { required: true });
 
 console.log(`Hello :), ${name}!`);
+
+// Get the JSON webhook payload for the event that triggered the workflow
+const payload = JSON.stringify(github.context.payload, undefined, 2)
+
+console.log(`The event payload: ${payload}`);
